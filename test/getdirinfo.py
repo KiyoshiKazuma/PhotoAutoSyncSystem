6136760a3b583293f5786309e34a358547d1fe42
KiyoshiKazuma <makiyoshikazu834@gmail.com>
2025-09-10 00:47:41 +0900

    マージ動作確認OK

--- test/getdirinfo.py
+++ test/getdirinfo.py
@@ -35,6 +35,8 @@ SYNC_AA = 4
 SYNC_DD = 5
 SYNC_EE = 6
 
+# デバッグモード
+DEBUG_LEVEL = 1
 
 # ファイル情報
 class file_info:
@@ -66,6 +68,8 @@ class composition_info:
 
     # ファイル情報を読み込む
     def read(self, input_file: str):
+        if DEBUG_LEVEL >= 2:
+            print(f"DEBUG:COMPOSITION_INFO: Reading composition info from {input_file}")
         with open(input_file, "r") as f:
             for line in f:
                 # 行からファイルパスとハッシュを抽出
@@ -78,6 +82,8 @@ class composition_info:
     
     # ファイル情報を書き込む
     def write(self, output_file: str):
+        if DEBUG_LEVEL >= 1:
+            print(f"DEBUG:COMPOSITION_INFO: Writing composition info to {output_file}")
         with open(output_file, "w") as f:
             for file in self.files:
                 f.write(f"File: {file.path}, Hash: {file.hash}\n")
@@ -88,12 +94,20 @@ class composition_info:
 
     def compare(self, otherfile: file_info):
         # ファイルの存在とハッシュ値の一致を確認
+        if DEBUG_LEVEL >= 2:
+            print(f"DEBUG: COMPOSITION_INFO: Comparing with {otherfile.path}")
         for file in self.files:
             if file.path == otherfile.path:
                 if file.hash == otherfile.hash:
+                    if DEBUG_LEVEL >= 2:
+                        print(f"DEBUG: COMPOSITION_INFO: the same file {file.path} in {otherfile.path} is found.")
                     return EQUAL
                 else:
+                    if DEBUG_LEVEL >= 2:
+                        print(f"DEBUG: COMPOSITION_INFO: the different file {file.path} in {otherfile.path} is found.")
                     return CHANGE
+        if DEBUG_LEVEL >= 2:
+            print(f"DEBUG: COMPOSITION_INFO: the file {otherfile.path} is not found.")
         return NOT_FOUND
 
 
@@ -131,11 +145,11 @@ class composition_diff_info:
                 
     # 指定のファイル情報が差分情報に含まれているか確認
     def diff_type(self, file: file_info) -> int:
-        if file in self.added_files:
+        if file.path in [f.path for f in self.added_files]:
             return ADD
-        elif file in self.removed_files:
+        elif file.path in [f.path for f in self.removed_files]:
             return REMOVE
-        elif file in self.changed_files:
+        elif file.path in [f.path for f in self.changed_files]:
             return CHANGE
         return EQUAL
 
@@ -219,17 +233,6 @@ class repository_info:
 
         self.composition_history = composition_diff_info()
         self.history_file_path = history_file_path
-        
-    # リポジトリ情報を更新
-    def update(self):
-        # リポジトリ内のファイルを走査し、構成情報を取得
-        self.get_repository_info()
-        
-        # 構成情報の差分を確認し、更新する
-        self.update_diff()
-        
-        # 構成情報をファイルに書き込む
-        self.write_repository_info()
 
     # リポジトリ内のファイルを走査し、構成情報を取得
     def get_repository_info(self):
@@ -271,9 +274,14 @@ class repository_info:
         # 変更前の構成情報を読み込む
         composition_pre = composition_info()
         composition_pre.read(self.composition_file_path)
-        
+
         # 構成情報の差分を取得し、更新
         self.composition_history.compare(composition_pre, self.composition)
+        
+        if DEBUG_LEVEL >= 1:
+            print(f"DEBUG: REPOSITORY_INFO: Repository {self.repository_root_path} diff updated.")
+            print("DEBUG: REPOSITORY_INFO: Current diff:")
+            self.composition_history.print()
         
         # 差分情報をファイルに書き込む
         self.write_diff()
@@ -294,23 +302,45 @@ class repository_info:
     # 差分情報をファイルから読み込む
     def read_diff(self):
         self.composition_history.read(self.history_file_path)
+        
+                
+    # リポジトリ情報を更新
+    def update(self):
+        # リポジトリ内のファイルを走査し、構成情報を取得
+        self.get_repository_info()
+        
+        # 構成情報の差分を確認し、更新する
+        self.update_diff()
+        
+        # 構成情報をファイルに書き込む
+        self.write_repository_info()
+        
+        if DEBUG_LEVEL >= 2:
+            print(f"DEBUG: REPOSITORY_INFO: Repository {self.repository_root_path} updated.")
+            print(f"DEBUG: REPOSITORY_INFO: Composition info written to {self.composition_file_path}")
+            print(f"DEBUG: REPOSITORY_INFO: Diff info written to {self.history_file_path}")
+            print("DEBUG: REPOSITORY_INFO: Current composition:")
+            self.composition.print_files()
+            print("DEBUG: REPOSITORY_INFO: Current diff:")
+            self.composition_history.print()
 
 
 
 
 class management_info:
     # プロパティの宣言
-    config_file_path: str                   # 設定ファイルのパス
-    repository1: repository_info            # リポジトリ1の情報
-    repository2: repository_info            # リポジトリ2の情報
-    repository1_root_path: str              # リポジトリ1のルートパス
-    repository2_root_path: str              # リポジトリ2のルートパス
-    repository1_repository_info_path: str   # リポジトリ1のファイルデータパス
-    repository2_repository_info_path: str   # リポジトリ2のファイルデータパス
+    config_file_path: str                    # 設定ファイルのパス
+    repository1: repository_info             # リポジトリ1の情報
+    repository2: repository_info             # リポジトリ2の情報
+    repository1_root_path: str               # リポジトリ1のルートパス
+    repository2_root_path: str               # リポジトリ2のルートパス
+    repository1_repository_info_path: str    # リポジトリ1のファイルデータパス
+    repository2_repository_info_path: str    # リポジトリ2のファイルデータパス
     repository1_history_path: str            # リポジトリ1の履歴データパス
     repository2_history_path: str            # リポジトリ2の履歴データパス
-    composition_diff: composition_diff_info # 構成情報の差分
-    back_up_folder_path: str               # バックアップフォルダのパス
+    composition_diff: composition_diff_info  # 構成情報の差分
+    back_up_folder_path: str                 # バックアップフォルダのパス
+    log_file_path: str                       # ログファイルのパス
 
     def __init__(self,config_file_path: str):
         self.config_file_path = config_file_path
@@ -339,6 +369,17 @@ class management_info:
             self.repository2_repository_info_path = lines[2].strip().split(": ")[2]
             self.repository2_history_path = lines[2].strip().split(": ")[3]
             self.back_up_folder_path = lines[4].strip().split(": ")[1]
+            self.log_file_path = lines[6].strip().split(": ")[1]
+            
+        if DEBUG_LEVEL >= 1:
+            print(f"DEBUG: MANAGEMENT_INFO: Reading config from {self.config_file_path}")
+            print(f"DEBUG: MANAGEMENT_INFO: Repository 1 root path: {self.repository1_root_path}")
+            print(f"DEBUG: MANAGEMENT_INFO: Repository 1 composition file path: {self.repository1_repository_info_path}")
+            print(f"DEBUG: MANAGEMENT_INFO: Repository 1 history path: {self.repository1_history_path}")
+            print(f"DEBUG: MANAGEMENT_INFO: Repository 2 root path: {self.repository2_root_path}")
+            print(f"DEBUG: MANAGEMENT_INFO: Repository 2 composition file path: {self.repository2_repository_info_path}")
+            print(f"DEBUG: MANAGEMENT_INFO: Repository 2 history path: {self.repository2_history_path}")
+            print(f"DEBUG: MANAGEMENT_INFO: Backup folder path: {self.back_up_folder_path}")
 
     # 2つのリポジトリの情報を更新
     def update(self):
@@ -348,17 +389,53 @@ class management_info:
     #    t2.start()
     #    t1.join()
     #    t2.join()
+        if DEBUG_LEVEL >= 1:
+            print("DEBUG: REPOSITORY_INFO: Updating repositories...")
         self.repository1.update()
         self.repository2.update()
     
     # 2つのリポジトリの構成情報を比較する
     def compare_repositories(self):
+        
         self.composition_diff.compare(self.repository1.composition, self.repository2.composition)
+        if DEBUG_LEVEL >= 1:
+            print("DEBUG: COMPOSITION_DIFF: Comparing repositories...")
+            self.composition_diff.print()
         
     # 2つのリポジトリの構成情報の差分をマージする
     def merge_diffs(self):
-        # 1 リポジトリ1の更新情報をマージ
-        # 1-1 追加されたファイルをマージ
+        # 1 リポジトリ間の差分情報をマージ
+        # 1-1 リポジトリ1のみに存在するファイルをマージ
+        for file in self.composition_diff.added_files:
+            # リポジトリ1,2の変更履歴に対象のファイル情報がない場合(変更履歴の取得漏れ or 初期差分)
+            # リポジトリ1にファイルをコピーする。
+            if (file.path not in [f.path for f in self.repository1.composition_history.added_files] and
+                file.path not in [f.path for f in self.repository1.composition_history.removed_files] and
+                file.path not in [f.path for f in self.repository1.composition_history.changed_files] and
+                file.path not in [f.path for f in self.repository2.composition_history.added_files] and
+                file.path not in [f.path for f in self.repository2.composition_history.removed_files] and
+                file.path not in [f.path for f in self.repository2.composition_history.changed_files]):
+                self.copy_file_repository2_to_1(file)
+        # 1-2 リポジトリ2のみに存在するファイルをマージ
+        for file in self.composition_diff.removed_files:
+            # リポジトリ1,2の変更履歴に対象のファイル情報がない場合(変更履歴の取得漏れ or 初期差分)
+            # リポジトリ2にファイルをコピーする。
+            if (file.path not in [f.path for f in self.repository1.composition_history.added_files] and
+                file.path not in [f.path for f in self.repository1.composition_history.removed_files] and
+                file.path not in [f.path for f in self.repository1.composition_history.changed_files] and
+                file.path not in [f.path for f in self.repository2.composition_history.added_files] and
+                file.path not in [f.path for f in self.repository2.composition_history.removed_files] and
+                file.path not in [f.path for f in self.repository2.composition_history.changed_files]):
+                self.copy_file_repository1_to_2(file)
+
+        # 1-3 リポジトリ1とリポジトリ2で差分があるファイル
+        for file in self.composition_diff.changed_files:
+            # リポジトリ1,2の変更履歴に対象のファイル情報がない場合
+            # コンフリクト
+            print(f"Conflict: File {file.path} changed in both repositories.")
+        
+        # 2 リポジトリ1の更新情報をマージ
+        # 2-1 追加されたファイルをマージ
         for file in self.repository1.composition_history.added_files:
             # 対象ファイルの差分情報を取得
             diff_type = self.composition_diff.diff_type(file)
@@ -381,7 +458,7 @@ class management_info:
                 # コンフリクト(リポジトリ1とリポジトリ2の両方で変更)
                 print(f"Conflict: File {file.path} changed in both repositories.")
 
-        # 1-2 削除されたファイルをマージ
+        # 2-2 削除されたファイルをマージ
         for file in self.repository1.composition_history.removed_files:
             # 対象ファイルの差分情報を取得
             diff_type = self.composition_diff.diff_type(file)
@@ -404,7 +481,7 @@ class management_info:
                 # コンフリクト(リポジトリ1で削除、リポジトリ2で変更)
                 print(f"Conflict: File {file.path} deleted in repository1 and changed in repository2.")                
                 
-        # 1-3 変更されたファイルをマージ
+        # 2-3 変更されたファイルをマージ
         for file in self.repository1.composition_history.changed_files:
             # 対象ファイルの差分情報を取得
             diff_type = self.composition_diff.diff_type(file)
@@ -444,8 +521,8 @@ class management_info:
                 # 更新情報を削除する
                 self.repository1.composition_history.remove_diff_file(file)
         
-        # 2 リポジトリ2の更新情報をマージ
-        # 2-1 追加されたファイルをマージ
+        # 3 リポジトリ2の更新情報をマージ
+        # 3-1 追加されたファイルをマージ
         for file in self.repository2.composition_history.added_files:
             # 対象ファイルの差分情報を取得
             diff_type = self.composition_diff.diff_type(file)
@@ -468,7 +545,7 @@ class management_info:
                 # コンフリクト(リポジトリ1とリポジトリ2の両方で変更)
                 print(f"Conflict: File {file.path} changed in both repositories.")
 
-        # 2-2 削除されたファイルをマージ
+        # 3-2 削除されたファイルをマージ
         for file in self.repository2.composition_history.removed_files:
             # 対象ファイルの差分情報を取得
             diff_type = self.composition_diff.diff_type(file)
@@ -489,9 +566,9 @@ class management_info:
             elif diff_type == CHANGE:
                 # リポジトリ2に削除されたファイルが、リポジトリ1のファイルと差分がある場合
                 # コンフリクト(リポジトリ1で変更、リポジトリ2で削除)
-                print(f"Conflict: File {file.path} deleted in repository1 and changed in repository2.")                
-                
-        # 2-3 変更されたファイルをマージ
+                print(f"Conflict: File {file.path} deleted in repository1 and changed in repository2.")
+
+        # 3-3 変更されたファイルをマージ
         for file in self.repository2.composition_history.changed_files:
             # 対象ファイルの差分情報を取得
             diff_type = self.composition_diff.diff_type(file)
@@ -530,7 +607,8 @@ class management_info:
                 # リポジトリ2に変更されたファイルが、リポジトリ1のみに存在する場合
                 # ありえないユースケースのためエラーログを出力。更新情報は保持する
                 print(f"Error: File {file.path} changed only in repository2. This should not happen.")
-                        
+                
+
 
     # 2つのリポジトリの情報と更新情報をファイルに書き込む
     def write(self):
@@ -538,53 +616,50 @@ class management_info:
         self.repository1.write_diff()
         self.repository2.write_repository_info()
         self.repository2.write_diff()
-
-    def copy_file_repository1_to_2(self, file: file_info):
-        src_path = os.path.join(self.repository1.repository_root_path, file.path)
-        dst_path = os.path.join(self.repository2.repository_root_path, file.path)
+        
+    def copy_file(self, src: str, dst: str, file: file_info):
+        src_path = os.path.join(src, file.path)
+        dst_path = os.path.join(dst, file.path)
         #　コピー先にファイルが存在する場合、上書きされるため、事前にバックアップを取得する
         if os.path.exists(dst_path):
-            self.move_file_repository2_to_backup(file)
+            if src == self.repository1.repository_root_path:
+                self.move_file_repository2_to_backup(file)
+            else:
+                self.move_file_repository1_to_backup(file)
         dst_dir = os.path.dirname(dst_path)
         if not os.path.exists(dst_dir):
             os.makedirs(dst_dir)
         print(f"Copying {src_path} to {dst_path}")
-        shutil.copy2(src_path, dst_path)
-        
-        
-    def copy_file_repository2_to_1(self, file: file_info):
-        src_path = os.path.join(self.repository2.repository_root_path, file.path)
-        dst_path = os.path.join(self.repository1.repository_root_path, file.path)
-        #　コピー先にファイルが存在する場合、上書きされるため、事前にバックアップを取得する
-        if os.path.exists(dst_path):
-            self.move_file_repository1_to_backup(file)
-        dst_dir = os.path.dirname(dst_path)
-        if not os.path.exists(dst_dir):
-            os.makedirs(dst_dir)
-        print(f"Copying {src_path} to {dst_path}")
+        #ログファイルにコピー情報を出力
+        with open(self.log_file_path, "a", encoding='utf-8') as log_file:
+            log_file.write(f"{datetime.now().strftime('%Y-%m-%d %H:%M:%S')} - Copying {src_path} to {dst_path}\n")
         shutil.copy2(src_path, dst_path)
     
-    def move_file_repository1_to_backup(self, file: file_info):
-        src_path = os.path.join(self.repository1.repository_root_path, file.path)
+    def move_file(self, src: str, file: file_info):
+        src_path = os.path.join(src, file.path)
         # バックアップ先のファイル名はタイムスタンプを付与する
         timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
-        dst_path = os.path.join(self.back_up_folder_path, "repository1", f"{timestamp}_{file.path}")
+        dst_path = os.path.join(self.back_up_folder_path, f"{timestamp}_{file.path}")
         dst_dir = os.path.dirname(dst_path)
         if not os.path.exists(dst_dir):
             os.makedirs(dst_dir)
-        print(f"Removing {src_path}. Backing up to {dst_path}")
+        print(f"Moving {src_path} to {dst_path}")
+        #ログファイルに移動情報を出力
+        with open(self.log_file_path, "a", encoding='utf-8') as log_file:
+            log_file.write(f"{datetime.now().strftime('%Y-%m-%d %H:%M:%S')} - Moving {src_path} to {dst_path}\n")
         shutil.move(src_path, dst_path)
+
+    def copy_file_repository1_to_2(self, file: file_info):
+        self.copy_file(self.repository1.repository_root_path, self.repository2.repository_root_path, file)
+        
+    def copy_file_repository2_to_1(self, file: file_info):
+        self.copy_file(self.repository2.repository_root_path, self.repository1.repository_root_path, file)
+    
+    def move_file_repository1_to_backup(self, file: file_info):
+        self.move_file(self.repository1.repository_root_path, file)
         
     def move_file_repository2_to_backup(self, file: file_info):
-        src_path = os.path.join(self.repository2.repository_root_path, file.path)
-        # バックアップ先のファイル名はタイムスタンプを付与する
-        timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
-        dst_path = os.path.join(self.back_up_folder_path, "repository2", f"{timestamp}_{file.path}")
-        dst_dir = os.path.dirname(dst_path)
-        if not os.path.exists(dst_dir):
-            os.makedirs(dst_dir)
-        print(f"Removing {src_path}. Backing up to {dst_path}")
-        shutil.move(src_path, dst_path)
+        self.move_file(self.repository2.repository_root_path, file)
 
 
 if __name__ == "__main__":
@@ -596,5 +671,7 @@ if __name__ == "__main__":
     management.compare_repositories()
     print("Merging diffs...")
     management.merge_diffs()
+    print("Update ")
+    management.update()
     print("Writing results...")
     management.write()
