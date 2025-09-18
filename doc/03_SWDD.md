afdea1b7ae1fd48b2106e00d109668830a0a0e45
KiyoshiKazuma <makiyoshikazu834@gmail.com>
2025-09-18 22:54:54 +0900

    コンフィグファイルの構成変更

--- doc/03_SWDD.md
+++ doc/03_SWDD.md
@@ -3,12 +3,60 @@
   - [2.1.API(外部接続)](#21api外部接続)
     - [2.1.1.提供API一覧](#211提供api一覧)
     - [2.1.2.呼び出しAPI](#212呼び出しapi)
-- [3.差分検出機能(DETECT)](#3差分検出機能detect)
-- [4.同期処理機能(SYNC)](#4同期処理機能sync)
-- [5.通知機能(NOTICE)](#5通知機能notice)
-- [6.データ管理機能(DATA)](#6データ管理機能data)
-- [7.コマンド操作機能(CMD)](#7コマンド操作機能cmd)
-  - [7.1.ファイル複製機能](#71ファイル複製機能)
+- [3. MANAGEMENT](#3-management)
+- [4. REPOSITORY](#4-repository)
+  - [4.1 概要](#41-概要)
+    - [概要 : リポジトリ情報](#概要--リポジトリ情報)
+    - [クラス名 : repository\_info](#クラス名--repository_info)
+    - [インスタンス定義](#インスタンス定義)
+  - [4.2 プロパティ](#42-プロパティ)
+  - [4.3 メソッド](#43-メソッド)
+    - [4.3.7 update](#437-update)
+      - [概要](#概要)
+      - [入力](#入力)
+      - [出力](#出力)
+      - [呼び出し関数](#呼び出し関数)
+      - [詳細](#詳細)
+    - [4.3.1 get\_repository\_info](#431-get_repository_info)
+      - [概要](#概要-1)
+      - [入力](#入力-1)
+      - [出力](#出力-1)
+      - [呼び出し関数](#呼び出し関数-1)
+      - [詳細](#詳細-1)
+    - [4.3.2 update\_diff](#432-update_diff)
+      - [概要](#概要-2)
+      - [入力](#入力-2)
+      - [出力](#出力-2)
+      - [呼び出し関数](#呼び出し関数-2)
+      - [詳細](#詳細-2)
+    - [4.3.3 write\_repository\_info](#433-write_repository_info)
+      - [概要](#概要-3)
+      - [入力](#入力-3)
+      - [出力](#出力-3)
+      - [呼び出し関数](#呼び出し関数-3)
+      - [詳細](#詳細-3)
+    - [4.3.4 read\_repository\_info](#434-read_repository_info)
+      - [概要](#概要-4)
+      - [入力](#入力-4)
+      - [出力](#出力-4)
+      - [呼び出し関数](#呼び出し関数-4)
+      - [詳細](#詳細-4)
+    - [4.3.5 write\_diff](#435-write_diff)
+      - [概要](#概要-5)
+      - [入力](#入力-5)
+      - [出力](#出力-5)
+      - [呼び出し関数](#呼び出し関数-5)
+      - [詳細](#詳細-5)
+    - [4.3.6 read\_diff](#436-read_diff)
+      - [概要](#概要-6)
+      - [入力](#入力-6)
+      - [出力](#出力-6)
+      - [呼び出し関数](#呼び出し関数-6)
+      - [詳細](#詳細-6)
+- [5. COMPOSITION](#5-composition)
+- [6. COMPOSITION\_DIFF](#6-composition_diff)
+- [7. MERGER](#7-merger)
+- [8. FILEOPERATER](#8-fileoperater)
 - [8.共通変数](#8共通変数)
 
 
@@ -63,32 +111,124 @@
 |戻り値   |設定データ|
 |備考     |-|
 
+# 3. MANAGEMENT
+
+# 4. REPOSITORY
+## 4.1 概要
+### 概要 : リポジトリ情報
+### クラス名 : repository_info
+### インスタンス定義
+repository_info(repository_root_path, composition_file_path, history_file_path)
 
 
-# 3.差分検出機能(DETECT)
+## 4.2 プロパティ
 
-
-
-
-# 4.同期処理機能(SYNC)
-# 5.通知機能(NOTICE)
-# 6.データ管理機能(DATA)
-# 7.コマンド操作機能(CMD)
-## 7.1.ファイル複製機能
-
-|要素|内容||
+|プロパティ名|型|概要|
 |:-|:-|:-|
-|機能概要|ファイルを複製する|
-|関数名|cmd_file_copy|
-|引数|src_repository|リポジトリ種別|
-|^   |src_filepath|文字列|
-|戻り値|std_ret|
-|備考|-|
+|repository_root_path|str|ディレクトリのルートパス|
+|composition_file_path|str|構成情報データのパス|
+|history_file_path|str|変更履歴情報のパス|
+|composition|composition_info|ディレクトリ内の構成情報|
+|composition_history|composition_diff_info|構成情報の差分履歴|
 
-[src_repository]リポジトリの[src_filepath]のファイルを対リポジトリの[src_filepath]に複製する。
-複製先に同じ名前のファイルがある場合は上書きする。
-複製に失敗した場合は[STD_NG]を返す。
-複製に成功した場合は[STD_OK]を返す。
+## 4.3 メソッド
+
+### 4.3.7 update
+#### 概要 
+リポジトリ情報を更新
+#### 入力 
+無し
+#### 出力 
+無し
+#### 呼び出し関数
+self.get_repository_info
+self.update_diff
+self.write_repository_info
+
+#### 詳細
+1. リポジトリ情報を取得
+2. リポジトリの変更履歴を更新
+3. 構成情報を書き出す
+
+リポジトリを走査して、構成情報と変更履歴を取得し、ファイルを更新する。
+
+### 4.3.1 get_repository_info
+#### 概要 
+走査してリポジトリ情報を取得する。
+#### 入力 
+self.repository_root_path
+#### 出力 
+self.composition
+#### 呼び出し関数
+count_files
+composition.clear
+get_file_hash
+composition.add_file
+#### 詳細
+1. compositionをリセットする。
+2. repository_root_path下のファイルを走査し、ファイル情報(ファイルの相対パスとハッシュ値)を取得する。
+3. ファイル情報をcompositonに設定する。
+1000ファイルごとに進捗を標準出力する。
+
+### 4.3.2 update_diff
+#### 概要 
+構成情報の差分を確認し、更新する
+#### 入力
+self.repository_root_path
+self.composition_file_path
+self.composition
+
+#### 出力
+self.composition_history
+
+#### 呼び出し関数
+self.read_diff
+composition.read
+composition_diff.compare
+self.write_diff
+
+#### 詳細
+1. history_file_pathのファイルから既存の変更履歴を読み出す。
+2. composition_file_pathのファイルから前回の構成情報を読み出す。
+3. 今回の構成情報と前回の構成情報を比較し、composition_historyを更新する。
+4. composition_historyをファイルhistory_file_pathに書き出す。
+
+### 4.3.3 write_repository_info
+#### 概要 
+構成情報をファイルに書き込む
+#### 入力 
+#### 出力 
+#### 呼び出し関数
+#### 詳細
+
+### 4.3.4 read_repository_info
+#### 概要 
+構成情報をファイルから読み込む
+#### 入力 
+#### 出力 
+#### 呼び出し関数
+#### 詳細
+
+### 4.3.5 write_diff
+#### 概要 
+差分情報をファイルに書き込む
+#### 入力 
+#### 出力 
+#### 呼び出し関数
+#### 詳細
+
+### 4.3.6 read_diff
+#### 概要 
+差分情報をファイルから読み込む
+#### 入力 
+#### 出力 
+#### 呼び出し関数
+#### 詳細
+
+# 5. COMPOSITION
+# 6. COMPOSITION_DIFF
+# 7. MERGER
+# 8. FILEOPERATER
 
 
 # 8.共通変数
--- src/PhotoAutoSync.py
+++ src/PhotoAutoSync.py
@@ -4,6 +4,9 @@ import time
 import hashlib
 from datetime import datetime
 import shutil
+import copy
+import csv
+import yaml
 
 # シンボル定義
 EQUAL = 0
@@ -31,6 +34,18 @@ SYNC_E = 3
 SYNC_AA = 4
 SYNC_DD = 5
 SYNC_EE = 6
+
+# コンフリクトの分類
+CONF_AA = 1 # サーバーリポジトリとクライアントリポジトリに追加されたファイルの中身が異なる
+CONF_EE = 2 # サーバーリポジトリとクライアントリポジトリで編集されたファイルの中身が異なる
+CONF_DE = 3 # サーバーリポジトリで削除されたがクライアントリポジトリで編集された
+CONF_ED = 4 # サーバーリポジトリで編集されたがクライアントリポジトリで削除された
+
+# プロセス番号
+PROCESS_DETECT_SERVER = 1
+PROCESS_DETECT_CLIENT = 2
+PROCESS_MERGE         = 3
+PROCESS_CONFLICT      = 4
 
 # デバッグモード
 DEBUG_LEVEL = 0
@@ -95,27 +110,30 @@ class composition_info:
 
     # ファイル情報を読み込む
     def read(self, input_file: str):
-        if DEBUG_LEVEL >= 2:
-            print(f"DEBUG:COMPOSITION_INFO: Reading composition info from {input_file}")
         # ファイルが存在するか確認
         if os.path.isfile(input_file):
             with open(input_file, "r") as f:
-                for line in f:
-                    # 行からファイルパスとハッシュを抽出
-                    if line.startswith("File:"): 
-                        line_split = line.split(", Hash:")
-                        file_path = line_split[0].replace("File: ", "").strip()
-                        file_hash = line_split[1].strip()
-                        file = file_info(file_path, file_hash)
-                        self.add_file(file)
+                reader = csv.reader(f)
+                for row in reader:
+                    if(len(row)) != 2:
+                        print(f"警告: 不正な形式の行をスキップします: {row}")
+                        continue
+                    file_path,file_hash = row
+                    file = file_info(file_path, file_hash)
+                    self.add_file(file)
+    
+    # 構成情報を初期化してファイル情報を読み出す
+    def clean_read(self, input_file: str):
+        self.clear()
+        self.read(input_file)
+        
     
     # ファイル情報を書き込む
     def write(self, output_file: str):
-        if DEBUG_LEVEL >= 1:
-            print(f"DEBUG:COMPOSITION_INFO: Writing composition info to {output_file}")
-        with open(output_file, "w") as f:
+        with open(output_file, "w", newline="") as f:
+            writer = csv.writer(f)
             for file in self.files:
-                f.write(f"File: {file.path}, Hash: {file.hash}\n")
+                writer.writerow([file.path,file.hash])
 
     def print_files(self):
         for file in self.files:
@@ -123,31 +141,26 @@ class composition_info:
 
     def compare(self, otherfile: file_info):
         # ファイルの存在とハッシュ値の一致を確認
-        if DEBUG_LEVEL >= 2:
-            print(f"DEBUG: COMPOSITION_INFO: Comparing with {otherfile.path}")
         for file in self.files:
             if file.path == otherfile.path:
                 if file.hash == otherfile.hash:
-                    if DEBUG_LEVEL >= 2:
-                        print(f"DEBUG: COMPOSITION_INFO: the same file {file.path} in {otherfile.path} is found.")
                     return EQUAL
                 else:
-                    if DEBUG_LEVEL >= 2:
-                        print(f"DEBUG: COMPOSITION_INFO: the different file {file.path} in {otherfile.path} is found.")
                     return CHANGE
-        if DEBUG_LEVEL >= 2:
-            print(f"DEBUG: COMPOSITION_INFO: the file {otherfile.path} is not found.")
         return NOT_FOUND
 
 
 # 構成情報の差分
 class composition_diff_info:
     # プロパティの宣言
-    added_files: list[file_info]
+    added_files:   list[file_info]
     removed_files: list[file_info]
     changed_files: list[file_info]
 
     def __init__(self):
+        self.clear()
+        
+    def clear(self):
         self.added_files = []
         self.removed_files = []
         self.changed_files = []
@@ -185,6 +198,7 @@ class composition_diff_info:
     def append_added_file(self, file: file_info):
         self.added_files.append(file)
 
+
     def append_removed_file(self, file: file_info):
         self.removed_files.append(file)
 
@@ -200,15 +214,13 @@ class composition_diff_info:
             self.changed_files.remove(file)
 
     def print(self):
-        print("Added files:")
+        print("Added:")
         for file in self.added_files:
             print(f"  {file.path}")
-
-        print("Removed files:")
+        print("Removed:")
         for file in self.removed_files:
             print(f"  {file.path}")
-
-        print("Changed files:")
+        print("Changed:")
         for file in self.changed_files:
             print(f"  {file.path}")
 
@@ -241,6 +253,10 @@ class composition_diff_info:
                         file = file_info(file_path, "")
                         self.append_changed_file(file)
 
+    def clean_read(self, input_file: str):
+        self.clear()
+        self.read(input_file)
+        
 # リポジトリ情報
 class repository_info:
     # プロパティの宣言
@@ -308,12 +324,7 @@ class repository_info:
 
         # 構成情報の差分を取得し、更新
         self.composition_history.compare(composition_pre, self.composition)
-        
-        if DEBUG_LEVEL >= 1:
-            print(f"DEBUG: REPOSITORY_INFO: Repository {self.repository_root_path} diff updated.")
-            print("DEBUG: REPOSITORY_INFO: Current diff:")
-            self.composition_history.print()
-        
+                
         # 差分情報をファイルに書き込む
         self.write_diff()
 
@@ -324,7 +335,10 @@ class repository_info:
     # 構成情報をファイルから読み込む
     def read_repository_info(self):
         self.composition.read(self.composition_file_path)
-
+        
+    # 構成情報をクリアしてファイルから読み込む
+    def clean_read_repository_info(self):
+        self.composition.clean_read(self.composition_file_path)
     
     # 差分情報をファイルに書き込む
     def write_diff(self):
@@ -333,8 +347,11 @@ class repository_info:
     # 差分情報をファイルから読み込む
     def read_diff(self):
         self.composition_history.read(self.history_file_path)
+    
+    # 差分情報をクリアしてファイルから読み込む
+    def clean_read_diff(self):
+        self.composition_history.clean_read(self.history_file_path)
         
-                
     # リポジトリ情報を更新
     def update(self):
         # リポジトリ内のファイルを走査し、構成情報を取得
@@ -346,17 +363,43 @@ class repository_info:
         # 構成情報をファイルに書き込む
         self.write_repository_info()
         
-        if DEBUG_LEVEL >= 2:
-            print(f"DEBUG: REPOSITORY_INFO: Repository {self.repository_root_path} updated.")
-            print(f"DEBUG: REPOSITORY_INFO: Composition info written to {self.composition_file_path}")
-            print(f"DEBUG: REPOSITORY_INFO: Diff info written to {self.history_file_path}")
-            print("DEBUG: REPOSITORY_INFO: Current composition:")
-            self.composition.print_files()
-            print("DEBUG: REPOSITORY_INFO: Current diff:")
-            self.composition_history.print()
+class merger:
+    # プロパティの宣言
+    repository1_root_path: str               # リポジトリ1のルートパス
+    repository2_root_path: str               # リポジトリ2のルートパス
+    repository1_repository_info_path: str    # リポジトリ1のファイルデータパス
+    repository2_repository_info_path: str    # リポジトリ2のファイルデータパス
+    repository1_history_path: str            # リポジトリ1の履歴データパス
+    repository2_history_path: str            # リポジトリ2の履歴データパス
+    back_up_folder_path: str                 # バックアップフォルダのパス
+    log_file_path: str                       # ログファイルのパス
+    
+    def __init__(self, repository1_root_path, repository2_root_path, repository1_repository_info_path, repository2_repository_info_path, repository1_history_path, repository2_history_path, back_up_folder_path, log_file_path):
+        self.repository1_root_path =repository1_root_path
+        self.repository2_root_path =repository2_root_path
+        self.repository1_repository_info_path = repository1_repository_info_path
+        self.repository2_repository_info_path = repository2_repository_info_path
+        self.repository1_history_path = repository1_history_path
+        self.repository2_history_path = repository2_history_path
+        self.back_up_folder_path = back_up_folder_path
+        self.log_file_path = log_file_path
+    
+    def merge(self):
+        print("do nothing")
 
+class conflict_info:
+    type: int
+    file_path: str    
 
-
+class conflict_manager:
+    # プロパティの宣言
+    conflict_log_path: str #コンフリクト情報のログ出力パス
+    conflict_list: list[conflict_info]
+    
+    def __init__(self, conflict_log_path):
+        self.conflict_log_path = conflict_log_path
+    
+    
 
 class management_info:
     # プロパティの宣言
@@ -392,49 +435,49 @@ class management_info:
 
     def readconfig(self):
         with open(self.config_file_path, "r", encoding='utf-8') as f:
-            lines = f.readlines()
-            self.repository1_root_path = lines[1].strip().split(": ")[1]
-            self.repository1_repository_info_path = lines[1].strip().split(": ")[2]
-            self.repository1_history_path = lines[1].strip().split(": ")[3]
-            self.repository2_root_path = lines[2].strip().split(": ")[1]
-            self.repository2_repository_info_path = lines[2].strip().split(": ")[2]
-            self.repository2_history_path = lines[2].strip().split(": ")[3]
-            self.back_up_folder_path = lines[4].strip().split(": ")[1]
-            self.log_file_path = lines[6].strip().split(": ")[1]
+            config_data =  yaml.safe_load(f)
+            self.repository1_root_path = config_data.get("repository1_root_path")
+            self.repository1_repository_info_path = config_data.get("repository1_repository_info_path")
+            self.repository1_history_path = config_data.get("repository1_history_path")
+            self.repository2_root_path = config_data.get("repository2_root_path")
+            self.repository2_repository_info_path = config_data.get("repository2_repository_info_path")
+            self.repository2_history_path = config_data.get("repository2_history_path")
+            self.back_up_folder_path = config_data.get("back_up_folder_path")
+            self.log_file_path = config_data.get("log_file_path")
             
-        if DEBUG_LEVEL >= 1:
-            print(f"DEBUG: MANAGEMENT_INFO: Reading config from {self.config_file_path}")
-            print(f"DEBUG: MANAGEMENT_INFO: Repository 1 root path: {self.repository1_root_path}")
-            print(f"DEBUG: MANAGEMENT_INFO: Repository 1 composition file path: {self.repository1_repository_info_path}")
-            print(f"DEBUG: MANAGEMENT_INFO: Repository 1 history path: {self.repository1_history_path}")
-            print(f"DEBUG: MANAGEMENT_INFO: Repository 2 root path: {self.repository2_root_path}")
-            print(f"DEBUG: MANAGEMENT_INFO: Repository 2 composition file path: {self.repository2_repository_info_path}")
-            print(f"DEBUG: MANAGEMENT_INFO: Repository 2 history path: {self.repository2_history_path}")
-            print(f"DEBUG: MANAGEMENT_INFO: Backup folder path: {self.back_up_folder_path}")
-
-    # 2つのリポジトリの情報を更新
-    def update(self):
-    #    t1 = threading.Thread(target=self.repository1.update)
-    #    t2 = threading.Thread(target=self.repository2.update)
-    #    t1.start()
-    #    t2.start()
-    #    t1.join()
-    #    t2.join()
-        if DEBUG_LEVEL >= 1:
-            print("DEBUG: REPOSITORY_INFO: Updating repositories...")
-        self.repository1.update()
-        self.repository2.update()
+    # 更新処理を実行する
+    # 引数:process_cntを指定すると途中から再開が可能
+    def process(self, process_cnt = PROCESS_DETECT_SERVER) :
+        # 1. 検出処理(サーバーリポジトリ)
+        if process_cnt == PROCESS_DETECT_SERVER:
+            self.repository1.update()
+            process_cnt += 1
+        
+        # 2. 検出処理(クライアントリポジトリ)
+        if process_cnt == PROCESS_DETECT_CLIENT:
+            self.repository2.update()
+            process_cnt += 1
+        
+        # 3. マージ処理
+        if process_cnt == PROCESS_MERGE:
+            management.compare_repositories()
+            self.merge_diffs()
+            self.write()
+            process_cnt += 1
+        
+        # 4. コンフリクト処理(TBD)
+        # do nothong        
     
     # 2つのリポジトリの構成情報を比較する
     def compare_repositories(self):
-        
+        # 構成情報をファイルから再読み込みする
+        self.clean_read()        
+        # 構成情報を比較
         self.composition_diff.compare(self.repository1.composition, self.repository2.composition)
-        if DEBUG_LEVEL >= 1:
-            print("DEBUG: COMPOSITION_DIFF: Comparing repositories...")
-            self.composition_diff.print()
         
     # 2つのリポジトリの構成情報の差分をマージする
     def merge_diffs(self):
+        '''
         # 1 リポジトリ間の差分情報をマージ
         # 1-1 リポジトリ1のみに存在するファイルをマージ
         for file in self.composition_diff.added_files:
@@ -464,16 +507,18 @@ class management_info:
             # リポジトリ1,2の変更履歴に対象のファイル情報がない場合
             # コンフリクト
             print(f"Conflict: File {file.path} changed in both repositories.")
-        
+        '''
         # 2 リポジトリ1の更新情報をマージ
         # 2-1 追加されたファイルをマージ
-        for file in self.repository1.composition_history.added_files:
+        
+        # ループ文中でインスタンスの要素を削除するため、リストのコピーを作成してループを回す
+        files = copy.deepcopy(self.repository1.composition_history.added_files)
+        for file in files:
             # 対象ファイルの差分情報を取得
             diff_type = self.composition_diff.diff_type(file)
-        
             if diff_type == EQUAL:
                 # 差分がない場合、マージは不要のため更新情報を削除する
-                self.repository1.composition_history.remove_diff_file(file)
+                self.repository1.composition_history.remove_diff_file(file)   
             elif diff_type == ADD:
                 # リポジトリ1に追加されたファイルが、リポジトリ2のみに存在する場合
                 # ありえないユースケースのためエラーログを出力。更新情報は保持する
@@ -487,10 +532,12 @@ class management_info:
             elif diff_type == CHANGE:
                 # リポジトリ1に追加されたファイルが、リポジトリ2のファイルと差分がある場合
                 # コンフリクト(リポジトリ1とリポジトリ2の両方で変更)
-                print(f"Conflict: File {file.path} changed in both repositories.")
+                print(f"Conflict: File {file.path} changed in both repositories.")            
 
         # 2-2 削除されたファイルをマージ
-        for file in self.repository1.composition_history.removed_files:
+        # ループ文中でインスタンスの要素を削除するため、リストのコピーを作成してループを回す
+        files = copy.deepcopy(self.repository2.composition_history.added_files)
+        for file in files:
             # 対象ファイルの差分情報を取得
             diff_type = self.composition_diff.diff_type(file)
         
@@ -648,6 +695,13 @@ class management_info:
         self.repository2.write_repository_info()
         self.repository2.write_diff()
         
+    # 2つのリポジトリの情報と更新情報をファイルから読み出す
+    def clean_read(self):
+        self.repository1.clean_read_repository_info()
+        self.repository1.clean_read_diff()
+        self.repository2.clean_read_repository_info()
+        self.repository2.clean_read_diff()
+        
     def copy_file(self, src: str, dst: str, file: file_info):
         src_path = os.path.join(src, file.path)
         dst_path = os.path.join(dst, file.path)
@@ -694,15 +748,6 @@ class management_info:
 
 
 if __name__ == "__main__":
-    config_file_path = ".\\config"
+    config_file_path = "C:/Users/makiy/programing/python/PhotoAutoSyncSystem/src/config"
     management = management_info(config_file_path)
-    print("Start updating repositories...")
-    management.update()
-    print("Comparing repositories...")
-    management.compare_repositories()
-    print("Merging diffs...")
-    management.merge_diffs()
-    print("Update ")
-    management.update()
-    print("Writing results...")
-    management.write()
+    management.process()
